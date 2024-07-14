78c9d5e6ceb0a9f5fd1d83f4937953e92afbaafa
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2024-07-14 20:06:27 +0100

    Stablecoin

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -26,7 +26,8 @@
 pragma solidity 0.8.19;
 
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
-
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 /**
  * @title DSC Engine
  * @author B. GHULLU
@@ -48,24 +49,30 @@ import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
  * @notice This contracct is VERY loosely based on the MakerDAO DSS (DAI) system.
  */
 
-contract DSCEngine {
-    ///////////////////
-    // Errors        //
-    ///////////////////
-
-    ////////////////////////
-    // State Variables    //
-    ////////////////////////
-    mapping(address token => address priceFeed) private s_priceFeeds;
-    DecentralizedStableCoin private immutable i_dsc;
-
+contract DSCEngine is ReentrancyGuard {
     ///////////////////
     // Errors        //
     ///////////////////
     error DSCEngine__AmountNeedsMoreThanZero();
     error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
     error DSC__NotAllowedToken();
-    
+    error DSC__TransferFailed();
+
+    ////////////////////////
+    // State Variables    //
+    ////////////////////////
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+    mapping(address user => uint256 amoutnDscMinted) private s_DSCMinted;
+
+    DecentralizedStableCoin private immutable i_dsc;
+
+    ///////////////////
+    //Events         //
+    ///////////////////
+
+    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
+
     ///////////////////
     // Modifiers     //
     ///////////////////
@@ -76,22 +83,17 @@ contract DSCEngine {
         _;
     }
 
-    modifier  isAllowedToken(address _token) {
-        if (s_priceFeeds[token]== address(0)){
+    modifier isAllowedToken(address _token) {
+        if (s_priceFeeds[_token] == address(0)) {
             revert DSC__NotAllowedToken();
         }
-        -;
-
+        _;
     }
 
     ///////////////////
     // Functions     //
     ///////////////////
-    constructor(
-        address[] memory tokenAddresses,
-        address[] memory priceFeedAddresses,
-        address dscAddress
-    ) {
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
         // USD Price Feeds
         if (tokenAddresses.length != priceFeedAddresses.length) {
             revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
@@ -109,24 +111,49 @@ contract DSCEngine {
     function depositCollateralAndMintDsc() external {}
 
     /**
-     *
+     * @notice follows CEI (check effects interactions)
      * @param tokenCollateralAddress The address of the token to deposit as collateral
      * @param amountCollateral The amount of collateral to deposit
      */
-    function depositCollateral(
-        address tokenCollateralAddress,
-        uint256 amountCollateral
-    ) external moreThanZero(amountCollateral) {}
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!success) {
+            revert DSC__TransferFailed();
+        }
+    }
 
     function redeemCollateralForDsc() external {}
 
     function redeemCollateral() external {}
 
-    function mintDsc() external {}
+    /**
+     *
+     * @notice follows CEI
+     * @param amountDscToMint The amount of DSC token to mint
+     * @notice they must have more collateral value than the minimum threshold
+     *
+     */
+    function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
+        s_DSCMinted[msg.sender] += amountDscToMint;
+        revertIfHealthFactorIsBroken();
+    }
 
     function burnDsc() external {}
 
     function liquidate() external {}
 
     function getHealthFactor() external view {}
+
+    //////////////////////////////////
+    // Private & Internal Functions //
+    //////////////////////////////////
+
+    function reverIfHealthFactorIsBroken() internal view {}
 }
